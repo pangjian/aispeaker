314218fc275f13d7071677ff64209f0ff9a3cffe
pangjian <pangjian1010@gmail.com>
2018-04-23 15:29:40 +0800

    no message

--- app/controller/exchange.js
+++ app/controller/exchange.js
@@ -10,12 +10,22 @@ class ExchangeController extends Controller {
     const req = ctx.request;
     const query = _.get(req.body, 'query'); // 用户说的话
     const requestType = _.get(req.body, 'request.type'); // Start,Intent,End
-    const rate = await ctx.service.exchange.getExchange();
     let message = '';
-    if (requestType === 1) {
-      message = messageBuilder.buildResponseSimple(query + rate, false);
+    if (requestType === 0) {
+      // 唤醒词请求
+      message = messageBuilder.buildResponseSimple('欢迎使用中行牌价', false);
+    } else if (requestType === 1) {
+      // 意图请求
+      // message = messageBuilder.buildResponse([ '这是一个例子', '播放完成后退出' ], true);
+      const slot_info = _.get(req.body, 'request.slot_info');
+      if (slot_info) {
+        if(request.slot_info.intent_name == 'query_exchange')
+        let rateObj = await ctx.service.exchange.getExchange(slot_info.slots[0].value);
+        message = messageBuilder.buildResponse([ '这是一个例子', '播放完成后退出' ], true);
+      }
     } else {
-      message = messageBuilder.buildResponse([ '这是一个例子', '播放完成后退出' ], true);
+      // 结束词请求
+
     }
     // this.ctx.body = rate;
     ctx.body = message;
--- app/service/exchange.js
+++ app/service/exchange.js
@@ -3,9 +3,30 @@
 const Service = require('egg').Service;
 
 class ExchangeService extends Service {
-  async getExchange() {
+  async getExchange(currency) {
     // PsnGetAllExchangeRateOutlay
-    return 'aaa';
+    const ctx = this.ctx;
+    const result = await ctx.curl('https://ccsa.ebsnew.boc.cn/BMPS/_bfwajax.do', {
+      method: 'POST',
+      data: {
+        json: JSON.stringify({
+          method: 'PsnGetExchangeOutlay',
+          params: {
+          },
+          header: {
+            agent: 'WEB15',
+          },
+        }),
+      },
+      dataType: 'json',
+    });
+    const res = result.data;
+    let currencyObj = {};
+    if (res.result) {
+
+      currencyObj = _.find(res.result, { curCode: currency });
+    }
+    return currencyObj;
   }
 }
 
